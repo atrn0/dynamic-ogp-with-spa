b2c414267607a1c1208ffa57723c35bbfe9d52b2
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-08-05 14:36:02 +0900

    simple server

--- ogp-proxy/index.ts
+++ ogp-proxy/index.ts
@@ -1,1 +1,12 @@
-console.log('hoge')
+import express from 'express'
+
+const app = express()
+const port = 8000
+
+app.get('*', (req, res) => {
+  res.send(req.url)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
