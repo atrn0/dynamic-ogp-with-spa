b526564c4354c385faa7a4e1d2025253b1ca04d7
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-08-05 11:56:36 +0900

    add react-router

---
+++ some-spa/src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom'
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <div>hoge</div>
+    </BrowserRouter>
+  );
+}
