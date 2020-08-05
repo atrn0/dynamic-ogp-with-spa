7b9e991fb41001b7f75a37a17b86caa62f14d23b
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-08-05 16:53:51 +0900

    display template engine

--- ogp-proxy/src/index.ts
+++ ogp-proxy/src/index.ts
@@ -1,10 +1,15 @@
 import express from 'express'
 
 const app = express()
+app.set('view engine', 'pug')
+
 const port = process.env.PORT || 8000
 
 app.get('*', (req, res) => {
-  res.send(req.url + ' from ogp proxy')
+  res.render('room', {
+    ogtitle: `${req.url} this is og:title!`,
+    path: req.url,
+  })
 })
 
 app.listen(port, () => {
